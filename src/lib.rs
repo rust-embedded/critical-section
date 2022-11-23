8a34ca1212099f453e112908119aea320978cd23
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-11-24 00:43:16 +0100

    Implement critical-section 0.2 in terms of 1.0.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![no_std]
-#![cfg_attr(target_arch = "avr", feature(llvm_asm))]
-#![cfg_attr(target_arch = "avr", feature(extended_key_value_attributes))]
 #![doc = include_str!("../README.md")]
 
+use core::convert::TryInto;
+use core::mem;
+
 pub use bare_metal::CriticalSection;
+
+const RESTORE_STATE_SIZE: usize = mem::size_of::<critical_section::RestoreState>();
 
 /// Acquire a critical section in the current thread.
 ///
@@ -22,11 +25,14 @@ pub use bare_metal::CriticalSection;
 /// - It is UB to call `release` with a restore token that does not come from the corresponding `acquire` call.
 #[inline]
 pub unsafe fn acquire() -> u8 {
-    extern "Rust" {
-        fn _critical_section_acquire() -> u8;
+    if RESTORE_STATE_SIZE > 1 {
+        // TODO do const assert instead?
+        panic!("restore state bigger than u8 is not supported.")
     }
 
-    _critical_section_acquire()
+    let state = critical_section::acquire();
+    let state: [u8; RESTORE_STATE_SIZE] = mem::transmute(state);
+    *state.get(0).unwrap_or(&0)
 }
 
 /// Release the critical section.
@@ -38,10 +44,11 @@ pub unsafe fn acquire() -> u8 {
 /// See [`acquire`] for the safety contract description.
 #[inline]
 pub unsafe fn release(token: u8) {
-    extern "Rust" {
-        fn _critical_section_release(token: u8);
-    }
-    _critical_section_release(token)
+    let state = [token];
+    let state: [u8; RESTORE_STATE_SIZE] = state[..RESTORE_STATE_SIZE].try_into().unwrap();
+    let state: critical_section::RestoreState = mem::transmute(state);
+
+    critical_section::release(state)
 }
 
 /// Execute closure `f` in a critical section.
@@ -50,149 +57,8 @@ pub unsafe fn release(token: u8) {
 /// are mostly no-ops since they're already protected by the outer one.
 #[inline]
 pub fn with<R>(f: impl FnOnce(CriticalSection) -> R) -> R {
-    unsafe {
-        let token = acquire();
-        let r = f(CriticalSection::new());
-        release(token);
-        r
-    }
+    critical_section::with(|_| f(unsafe { CriticalSection::new() }))
 }
 
-cfg_if::cfg_if! {
-    if #[cfg(feature = "custom-impl")] {
-        /// Methods required for a custom critical section implementation.
-        ///
-        /// This trait is not intended to be used except when implementing a custom critical section.
-        ///
-        /// Implementations must uphold the contract specified in [`crate::acquire`] and [`crate::release`].
-        #[cfg_attr(docsrs, doc(cfg(feature = "custom-impl")))]
-        pub unsafe trait Impl {
-            /// Acquire the critical section.
-            unsafe fn acquire() -> u8;
-            /// Release the critical section.
-            unsafe fn release(token: u8);
-        }
-
-        /// Set the custom critical section implementation.
-        ///
-        /// # Example
-        ///
-        /// ```
-        /// struct MyCriticalSection;
-        /// critical_section::custom_impl!(MyCriticalSection);
-        ///
-        /// unsafe impl critical_section::Impl for MyCriticalSection {
-        ///     unsafe fn acquire() -> u8 {
-        ///         // ...
-        ///         # return 0
-        ///     }
-        ///
-        ///     unsafe fn release(token: u8) {
-        ///         // ...
-        ///     }
-        /// }
-        ///
-        #[cfg_attr(docsrs, doc(cfg(feature = "custom-impl")))]
-        #[macro_export]
-        macro_rules! custom_impl {
-            ($t: ty) => {
-                #[no_mangle]
-                unsafe fn _critical_section_acquire() -> u8 {
-                    <$t as $crate::Impl>::acquire()
-                }
-                #[no_mangle]
-                unsafe fn _critical_section_release(token: u8) {
-                    <$t as $crate::Impl>::release(token)
-                }
-            };
-        }
-    } else if #[cfg(cortex_m)] {
-        #[no_mangle]
-        unsafe fn _critical_section_acquire() -> u8 {
-            let primask = cortex_m::register::primask::read();
-            cortex_m::interrupt::disable();
-            primask.is_active() as _
-        }
-
-        #[no_mangle]
-        unsafe fn _critical_section_release(token: u8) {
-            if token != 0 {
-                cortex_m::interrupt::enable()
-            }
-        }
-    } else if #[cfg(target_arch = "avr")] {
-        #[no_mangle]
-        unsafe fn _critical_section_acquire() -> u8 {
-            let mut sreg: u8;
-            llvm_asm!(
-                "in $0, 0x3F
-                 cli"
-                : "=r"(sreg)
-                ::: "volatile"
-            );
-            sreg
-        }
-
-        #[no_mangle]
-        unsafe fn _critical_section_release(token: u8) {
-            if token & 0x80 == 0x80 {
-                llvm_asm!("sei" :::: "volatile");
-            }
-        }
-    } else if #[cfg(target_arch = "riscv32")] {
-        #[no_mangle]
-        unsafe fn _critical_section_acquire() -> u8 {
-            let interrupts_active = riscv::register::mstatus::read().mie();
-            riscv::interrupt::disable();
-            interrupts_active as _
-        }
-
-        #[no_mangle]
-        unsafe fn _critical_section_release(token: u8) {
-            if token != 0 {
-                riscv::interrupt::enable();
-            }
-        }
-    } else if #[cfg(any(unix, windows, wasm, target_arch = "wasm32"))] {
-        extern crate std;
-        use std::sync::{Once, Mutex, MutexGuard};
-        use core::cell::Cell;
-
-        static INIT: Once = Once::new();
-        static mut GLOBAL_LOCK: Option<Mutex<()>> = None;
-        static mut GLOBAL_GUARD: Option<MutexGuard<'static, ()>> = None;
-
-        std::thread_local!(static IS_LOCKED: Cell<bool> = Cell::new(false));
-
-        #[no_mangle]
-        unsafe fn _critical_section_acquire() -> u8 {
-            INIT.call_once(|| unsafe {
-                GLOBAL_LOCK.replace(Mutex::new(()));
-            });
-
-            // Allow reentrancy by checking thread local state
-            IS_LOCKED.with(|l| {
-                if !l.get() {
-                    let guard = GLOBAL_LOCK.as_ref().unwrap().lock().unwrap();
-                    GLOBAL_GUARD.replace(guard);
-                    l.set(true);
-                    1
-                } else {
-                    0
-                }
-            })
-        }
-
-        #[no_mangle]
-        unsafe fn _critical_section_release(token: u8) {
-            if token == 1 {
-                GLOBAL_GUARD.take();
-                IS_LOCKED.with(|l| {
-                    l.set(false);
-                });
-            }
-        }
-    } else {
-        compile_error!("Critical section is not implemented for this target. Make sure you've specified the correct --target. You may need to supply a custom critical section implementation with the `custom-impl` feature");
-    }
-}
+#[cfg(feature = "custom-impl")]
+pub use critical_section::{set_impl as custom_impl, Impl};
